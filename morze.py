430fc208eb286f066a88e31b7c685bfd77698ce6
Bogotron <38692407+Bogotron@users.noreply.github.com>
2018-08-06 18:13:19 +0300

    Create morze.py
    First "morze" try

---
+++ morze.py
@@ -0,0 +1,18 @@
+# AZBUKA MORZE
+import string
+
+def morze(word):
+    alphabet = []
+    morze_alphabet = ['*-', '-***', '-*-*', '-**', '*', '**-*', '--*', '****', '**', '*---', '-*-', '*-**', '--',
+                      '-*', '---', '*--*', '--*-', '*-*', '***', '-', '**-', '***-', '*--', '-**-', '-*--', '--**']
+    return_phrase = []
+    for letter in string.ascii_uppercase:
+        alphabet.append(letter)
+    morze_dict = dict(zip(alphabet, morze_alphabet))
+
+    key_values = lambda 
+    print(key_values)
+    for key in key_values:
+        pass
+
+morze('hello')
