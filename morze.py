3d53c71177f90b3dae5230c7dec15ee657d9f027
Bogo <bogotronic@gmail.com>
2018-08-09 23:52:54 +0300

    morze update

--- morze.py
+++ morze.py
@@ -1,21 +1,27 @@
 # AZBUKA MORZE
 import string
 
-def morze(word):
+def morze_word(word):
     alphabet = []
     morze_alphabet = ['*-', '-***', '-*-*', '-**', '*', '**-*', '--*', '****', '**', '*---', '-*-', '*-**', '--',
                       '-*', '---', '*--*', '--*-', '*-*', '***', '-', '**-', '***-', '*--', '-**-', '-*--', '--**']
     for letter in string.ascii_uppercase:
         alphabet.append(letter)
     morze_dict = dict(zip(alphabet, morze_alphabet))
-    print(morze_dict)
 
     code_phrase = []
     for letter in word:
-        code_phrase.append(letter.upper())
+        if letter.upper() in string.ascii_uppercase:
+            key = letter.upper()
+            code_phrase.append(morze_dict[key])
+        else:
+            break
     print(code_phrase)
-    print(return_phrase)
 
-    return_phrase = []
-    
-morze('hello')
+def morze_phrase(*args):
+    morze_list = list(map(morze_word, args))
+    return morze_list
+
+morze_phrase('Hello', 'world')
+
+'Надо добавить обработку пробела для функции morze_word'
