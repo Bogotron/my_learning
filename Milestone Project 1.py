d45c618cfe92f81d4c8a43dc36ba6ff41d5d0fc6
Bogo <bogotronic@gmail.com>
2018-08-22 00:15:29 +0300

    morze update

---
+++ Milestone Project 1.py
@@ -0,0 +1,15 @@
+# Milestone Project 1
+
+#import clear display function
+from
+
+# Player choice
+while True:
+    player_1 = input('Please enter "X" or "O" to choice your side [type "Q" to exit]: ')
+    if player_1.upper() == "X":
+        print('Player 1 is "X"')
+    elif player_1.upper() == "O":
+        print('Player 1 is "O"')
+    elif player_1.upper() == "Q":
+        break
+
