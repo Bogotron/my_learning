2c27a6f4280944c2af9df203dc0c0e38a0a3e55e
Bogotron <bogotronic@gmail.com>
2019-09-16 23:27:00 +0300

    TTT Update

--- my_learning/New_tictactoe.py
+++ my_learning/New_tictactoe.py
@@ -14,26 +14,12 @@ def side_choice():
     while marker != "X" and marker != "O":
         marker = input('Player one please choise side X or O: ').upper()
         if marker == "X":
+            print(marker[0], "will turn first")
             return ("X", "O")
         elif marker == "O":
+            print(marker[0], "will turn first")
             return ("O", "X")
-
-def turn(player_choise):
-    turn_number = 0
-    while turn_number < 9:
-        try:
-            marker = int(input('Enter your field position 1-9: '))
-            if field[marker] == " ":
-                field[marker] = player_choise[turn_number%2]
-                print("Next player")
-                print(field[0] + field[1] + field[2])
-                print(field[3] + field[4] + field[5])
-                print(field[6] + field[7] + field[8])
-                turn_number += 1
-        except ValueError and IndexError:
-            print("Enter your field position please!")               
-
-
+        
 def victory_check(field, marker):
     return (marker in (field[0] and field[1] and field [2]) or 
             marker in (field[3] and field[4] and field [5]) or
@@ -45,12 +31,40 @@ def victory_check(field, marker):
             marker in (field[2] and field[4] and field [6])
             )
 
+
+def turn(player_choise):
+    turn_number = 0
+    while " " in field:
+        try:
+            marker_allocation = int(input('Enter your field position 1-9: '))
+            if field[marker_allocation] == " ":
+                field[marker_allocation] = player_choise[turn_number%2] # define who turn now O or X
+                if victory_check(field, field[marker_allocation]) == True:
+                    print(field[marker_allocation], "WINNER!!!")
+                    break
+                print('debug: ', victory_check(field, field[marker_allocation]))
+                print("Next player", field[marker_allocation]) # Move to game field
+                print(field[0] + field[1] + field[2]) # Move to game field
+                print(field[3] + field[4] + field[5]) # Move to game field
+                print(field[6] + field[7] + field[8]) # Move to game field
+                turn_number += 1
+        except ValueError and IndexError:
+            print("Enter your field position please!")
+
+
+
 def game_field():
     pass
 
-
+def another_game():
+    pass
 #MAIN GAME
 
-if start_game() == True:
-    side_choice()
-    turn()
+turn(("O", "X"))
+
+'''
+while start_game() == True:
+    players = side_choice()
+    while victory_check(field, players[0]) == False or victory_check(field, players[1]) == False:
+        turn(players)
+'''
