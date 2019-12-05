28df69a28b9279ae298f9620c45cabbb3b262ee0
Bogotron <bogotronic@gmail.com>
2019-12-05 18:23:54 +0300

    Need fix and clarify

--- Trade_result_parce/Month_separating.py
+++ Trade_result_parce/Month_separating.py
@@ -10,17 +10,12 @@ def file_separater (file):
     source_file = open(file,"r")
     header = source_file.readline() # write in first line in each new separated file
     counter = 0
-    print(header, end='')
     buffer = source_file.readline() #buffer for comparator
-    print(buffer.split(';'))
     file_name = str(buffer.split(';')[3][:7] + '.csv') #variable for separated file name
-    print(buffer.split(';')[3][:7])
-    
     for line in source_file:
         if line.split(';')[3][:7] == buffer.split(';')[3][:7]:
             try:
                 result_file = open(buffer.split(';')[3][:7]+'.csv', 'a')
-                print(file_name + ' ' + buffer.split(';')[3][:7])
                 result_file.write(line)
             finally:
                 result_file.close()
