9692379dc84063070f70c5d112adfe79ba538f21
Bogotron <bogotronic@gmail.com>
2019-12-05 18:18:01 +0300

    Нужна доработка в плане логики, вычистить лишнее.

--- Trade_result_parce/Month_separating.py
+++ Trade_result_parce/Month_separating.py
@@ -12,16 +12,21 @@ def file_separater (file):
     counter = 0
     print(header, end='')
     buffer = source_file.readline() #buffer for comparator
-    file_name = str(buffer.split(',')[3][:7] + '.csv') #variable for separated file name
-    print(buffer.split(',')[3][:7])
-    print(file_name)
+    print(buffer.split(';'))
+    file_name = str(buffer.split(';')[3][:7] + '.csv') #variable for separated file name
+    print(buffer.split(';')[3][:7])
     
     for line in source_file:
-        if line.split(',')[3][:7] == buffer.split(',')[3][:7]:
-            print(line, end='')
-        elif line.split(',')[3][:7] != buffer.split(',')[3][:7]:
+        if line.split(';')[3][:7] == buffer.split(';')[3][:7]:
+            try:
+                result_file = open(buffer.split(';')[3][:7]+'.csv', 'a')
+                print(file_name + ' ' + buffer.split(';')[3][:7])
+                result_file.write(line)
+            finally:
+                result_file.close()
+        elif line.split(';')[3][:7] != buffer.split(';')[3][:7]:
             buffer = line
     source_file.close() # close source_file
     
     
-file_separater('head100_traderes.csv')
+file_separater('TradeResultFull.csv')
